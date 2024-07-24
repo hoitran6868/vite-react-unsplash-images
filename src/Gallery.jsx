3f9acfba38dc2ac6961a91ed15bf9904ddf62c03
hoitran6868 <abc@example.com~clear>
2024-07-24 21:01:43 +0900

    Setup global searchvalue and Fix useQuery

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,14 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import { useGlobalContext } from './context';
 
 const url =
-  'https://api.unsplash.com/search/photos?client_id=EmJqCWp9-fhPRaXMP4y5Tw4Ts-U3QkdETl3xW_WKn88&query=cat';
+  'https://api.unsplash.com/search/photos?client_id=EmJqCWp9-fhPRaXMP4y5Tw4Ts-U3QkdETl3xW_WKn88';
 
 const Gallery = () => {
+  const { searchTerm } = useGlobalContext();
+
   const response = useQuery({
-    queryKey: ['images'],
+    queryKey: ['images', searchTerm],
     queryFn: async () => {
-      const result = await axios.get(url);
+      const result = await axios.get(`${url}&query=${searchTerm}`);
 
       return result.data;
     },
